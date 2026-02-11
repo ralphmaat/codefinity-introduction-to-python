e9c82ec9f16bdad6db9626541009e9faa700ae01
ralphmaat <>
2026-02-11 11:34:50 +0000

    Challenge: Automating Stock Control

--- loops/challenge_automating_stock_control/main.py
+++ loops/challenge_automating_stock_control/main.py
@@ -11,16 +11,28 @@ discount_threshold = 100
 print("Processing started")
 
 for item in inventory:
+    print(f"Processing {item}")
     current_stock = inventory[item][0]
     minimum_stock = inventory[item][1]
     restock_quant = inventory[item][2]
     sale_status = inventory[item][3]
-    print(current_stock)
-    print(minimum_stock)
-    print(restock_quant)
-    print(sale_status)          
+    #print("Item ",item)
+    #print("Current stock: ",current_stock)
+    #print("Minimum stock: ",minimum_stock)
+    #print("Restock quant.: ",restock_quant)
+    #print("Sales status: ", sale_status)
+    while (current_stock < minimum_stock):
+        inventory[item][0] += restock_quant
+        current_stock = inventory[item][0]
+        print(inventory)
+    if current_stock > discount_threshold and not sale_status:
+        inventory[item][3] = True
+    inventory[item][0] = current_stock
+print("Processing completed")
 
-
+  
+        
+    
 
     
     
